eec174f7aa86be8b275f8e0f76a746b1b7ff4bb0
Rodolphe <rodolpheandrieux@gmail.com>
2024-04-07 17:35:46 +0200

    canvas to custom

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
+	import './styles.css';
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+	.app {
+		position: relative;
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	.app::before {
+		content: "";
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		background-image: url("src/lib/images/noise.png");
+		background-size: 10%;
+		background-repeat: repeat;
+		opacity: 0.05;
+		z-index: -1;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/Footer.svelte
@@ -0,0 +1,39 @@
+<footer>
+    <img id="ynov-footer-logo" src="src/lib/images/ynov-logo.png" alt="Logo d'ynov" height="100">
+    <div>
+        <p>© 2023 - Rodolphe ANDRIEUX</p>
+        <a href="https://www.linkedin.com/in/rodolphe-andrieux" target="_blank">
+            <img src="src/lib/images/linkedin.svg" alt="Logo LinkedIn" width="30" height="30">
+        </a>
+        <a href="https://github.com/RodolpheANDRIEUX" target="_blank">
+            <img src="src/lib/images/github.svg" alt="Logo GitHub" width="30" height="30">
+        </a>
+    </div>
+</footer>
+
+<style>
+    footer {
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+        background-color: #2e2e2e;
+        color: white;
+        padding-bottom: 50px;
+    }
+
+    footer div {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    footer div * {
+        margin-bottom: 10px;
+    }
+
+    footer #ynov-footer-logo {
+        background: white;
+    }
+</style>
